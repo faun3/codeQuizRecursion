df1a9ee9d233825d897922d2732662086d735194
faun3 <antonioculcea@gmail.com>
2023-05-07 15:49:52 +0300

    finish exercise 9

---
+++ functions/replicate.js
@@ -0,0 +1,10 @@
+console.log(replicate(3, 5)); // [5, 5, 5]
+console.log(replicate(1, 69)); // [69]
+console.log(replicate(-2, 6)); // []
+
+function replicate(times, number) {
+  if (times <= 0) return [];
+  else {
+    return [number].concat(replicate(times - 1, number));
+  }
+}
